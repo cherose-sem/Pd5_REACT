7c5094065697037523115b1575ee9db02654c898
UsernameDiana <ms.strele@gmail.com>
2017-05-11 17:33:10 +0200

    changed inputfield in newBook to form

--- routerDemo/period5startcode/src/NewBook.js
+++ routerDemo/period5startcode/src/NewBook.js
@@ -3,21 +3,45 @@ import { observer } from "mobx-react"
 
 @observer
 export default class NewBook extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      book: { title: "", info: "", moreInfo: "" }
+    };
+  }
+
+  handleSubmit = (evt) => {
+    evt.preventDefault();
+  }
+
+  handleInput = (event) => {
+    const target = event.target;
+    const prop = target.id;
+    var value = target.value;
+    var book = this.state.book;
+    book[prop] = value;
+    this.setState({
+      book: book
+    });
+  }
+
   render() {
-    const bookStore = this.props.route.bookStore;
     return (
       <div>
-        <h2> YOU CAN NOW ADD YOUR OWN BOOK!!! (^.^) </h2>
-        <br/>
-        <br/>
-        <input id="title" type="text" placeholder="Title" />
-        <br/>
-        <input id="info" type="text" placeholder="Info" />
-        <br/>
-        <input id="moreInfo" type="text" placeholder="More info" />
-        <br/>
-        <br/>
-        <button onClick={() => bookStore.newBook(document.getElementById("title"), document.getElementById("info"), document.getElementById("moreInfo"))}>ADD</button>
+      <h2>ADD YOUR BOOK HERE (^.^)</h2>
+        <form onSubmit={this.handleSubmit} >
+          <input id="title" type="text" value={this.state.book.title}
+            placeholder="Title" onChange={this.handleInput} />
+          <br />
+          <input id="info" type="text" value={this.state.book.info}
+            placeholder="Info" onChange={this.handleInput} />
+          <br />
+          <input id="moreInfo" type="text" value={this.state.book.moreInfo}
+            placeholder="MoreInfo" onChange={this.handleInput} />
+          <br />
+          <button >Submit</button>
+        </form>
       </div>
     );
   }
