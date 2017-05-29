26adb3ec669352c9ab9f3fad349bb19f2031b9da
Cherry Rose Semeña <Cherry Rose Semea>
2017-05-29 12:13:56 +0200

    fixed backend issues

--- routerDemo/period5startcode/backend/db.js
+++ routerDemo/period5startcode/backend/db.js
@@ -1,78 +1,86 @@
 var MongoClient = require('mongodb').MongoClient
 var assert = require('assert')
-var url = "mongodb://cph-cs241:cjs192@ds017514.mlab.com:17514/cjs_db"
+// var url = "mongodb://cph-cs241:cjs192@ds017514.mlab.com:17514/cjs_db"
+var url = 'mongodb://127.0.0.1/booksDB'
+var autoIncrement = require("mongodb-autoincrement");
 
 
-function setURL(newURL) {
-  url = newURL
+function setURL(newURL){
+    url = newURL
 }
 
-function getBooks(callback) {
-  MongoClient.connect(url, function (err, db) {
-    assert.equal(null, err)
-    assert.ok(db != null)
+function getBooks(callback){
+    MongoClient.connect(url, function(err,db){
+        assert.equal(null,err)
+        assert.ok(db != null)
 
-    db.collection("books").find({}).toArray(function (err, data) {
-      assert.equal(null, err)
-      var books = data
-      callback(books)
-      db.close()
+        db.collection("books").find({}).toArray(function(err,data){
+            assert.equal(null,err)
+            var books = data
+            callback(books)
+            db.close()
+        })
     })
-  })
 }
 
-function addBook(book, callback) {
-  console.log(book)
-  MongoClient.connect(url, function (err, db) {
-      var collection = db.collection("books")
-      collection.insert(book, function (err, data) {
-        assert.equal(null, err)
-        callback(data)
-      })
+function addBook(book, callback){
+    console.log(book)
+    MongoClient.connect(url, function(err,db){
+        assert.equal(null,err)
+        assert.ok(db != null)
+        autoIncrement.getNextSequence(db, "books", function (err, autoIndex) {
+                var collection = db.collection("books")
+                book.id = autoIndex
+                collection.insertOne({id: book.id, title: book.title, info: book.info, moreInfo: book.moreInfo}, function(err,data){
+                    assert.equal(null,err)
+                    var result = data.ops[0]
+                    callback(result)
+                })
+            })
+
+        })
+    
+}
+
+function deleteBook(bookId, callback){
+    MongoClient.connect(url, function(err,db){
+        assert.equal(null,err)
+        assert.ok(db != null)
+
+        db.collection("books").deleteOne({id: bookId}, function(err,data){
+            assert.equal(null,err)
+            var response = data
+            callback(response)
+        })
     })
-
-
 }
 
-function deleteBook(bookId, callback) {
-  MongoClient.connect(url, function (err, db) {
-    assert.equal(null, err)
-    assert.ok(db != null)
+function updateBook(book,callback){
+    MongoClient.connect(url,function(err,db){
+        assert.equal(null,err)
+        assert.ok(db != null)
+        var options = {
+            returnOriginal : false,
+            upsert: true
+        }
 
-    db.collection("books").deleteOne({ _id: bookId }, function (err, data) {
-      assert.equal(null, err)
-      var response = data
-      callback(response)
+        db.collection("books").findOneAndReplace({id: book.id},
+                    {$set: {"id": book.id, "title": book.title, "info": book.info, "moreInfo": book.moreInfo}},
+                    options,
+                            function(err,data){
+                            assert.equal(null,err)
+                            var updatedBook = data.value
+                            callback(updatedBook)
+                            })
     })
-  })
 }
 
-function updateBook(book, callback) {
-  MongoClient.connect(url, function (err, db) {
-    assert.equal(null, err)
-    assert.ok(db != null)
-    var options = {
-      returnOriginal: false,
-      upsert: true
-    }
-
-    db.collection("books").findOneAndReplace({ _id: book.id },
-      { $set: { "_id": book.id, "title": book.title, "info": book.info, "moreInfo": book.moreInfo } },
-      options,
-      function (err, data) {
-        assert.equal(null, err)
-        var updatedBook = data.value
-        callback(updatedBook)
-      })
-  })
+var facade = {
+    getBooks: getBooks,
+    addBook : addBook,
+    deleteBook : deleteBook,
+    updateBook : updateBook,
+    setURL : setURL
 }
 
-var crud = {
-  getBooks: getBooks,
-  addBook: addBook,
-  deleteBook: deleteBook,
-  updateBook: updateBook,
-  setURL: setURL
-}
-
-module.exports = crud
+module.exports = facade
